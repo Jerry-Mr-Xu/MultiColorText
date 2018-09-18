da1feef195d4186f30959720b0b4c7f14e1dbef1
Jerry <285373161@qq.com>
2018-09-18 14:49:23 +0800

    Build project

---
+++ app/src/main/java/com/jerry/multicolortext/MainActivity.java
@@ -0,0 +1,18 @@
+package com.jerry.multicolortext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 演示页面
+ *
+ * @author xujierui
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
